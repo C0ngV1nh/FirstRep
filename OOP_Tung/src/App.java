6c337228287c39ab8133e39673c3763d25f85411
C0ngV1nh <24681851.vinh@student.iuh.edu.vn>
2025-08-14 21:28:04 +0700

    Commit from VSC

--- OOP_Tung/src/App.java
+++ OOP_Tung/src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Nguyen Van Cong Vinh!");
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.print("Nhập n: ");
+        int n = scanner.nextInt();
+
+        int sum = 0;
+       for(int i = 1 ; i <= n ; i++){
+            sum+=i;
+       }
+        System.out.println("tổng các số từ 1 đến n: " + sum);
+
     }
 }
